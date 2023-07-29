f7b9e2ee2fc457282eba1e61a6e707cb31dbe9b1
Paras Saxena <paras.saxena@qodeit.space>
2023-07-29 17:18:58 +0530

    Frontend - backend communication setup

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://backend-qodeit.onrender.com",
+});
 
 export const apiSlice = createApi({
   baseQuery,
