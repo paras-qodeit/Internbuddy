98351570dd1b8b008f4bd5b97ac9bafab94c5cea
Paras Saxena <paras.saxena@qodeit.space>
2023-08-05 10:18:21 +0530

    local

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
-  // baseUrl: "https://backend-qodeit.onrender.com",
+  // baseUrl: "http://localhost:5000",
+  baseUrl: "https://backend-qodeit.onrender.com",
 });
 
 export const apiSlice = createApi({
